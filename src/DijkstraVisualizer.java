d9ad1eb9e6868375782bdc889b6931077ee606e6
SantiagoSierraA <santiagosierra.arco@gmail.com>
2025-06-03 21:31:16 -0400

    feat: implement cell interaction logic for wall placement

--- src/DijkstraVisualizer.java
+++ src/DijkstraVisualizer.java
@@ -1,14 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class DijkstraVisualizer {
     JFrame frame  = new JFrame("Dijkstra Visualizer");
     JPanel gridPanel = new JPanel();
-    JButton[][] cells;
+    JButton[][] cells = new JButton[10][10];
     int startRow = 1;
     int startCol = 1;
     int endRow = 8;
     int endCol = 8;
+
+    Color wallColor = new Color(21, 21, 20);
+    Color cellColor = new Color(245, 249, 228);
+
+    boolean isDragging = false;
+    boolean isAddingWalls = true;
 
     DijkstraVisualizer() {
         // Frame
@@ -28,10 +36,8 @@ public class DijkstraVisualizer {
     }
 
     void createGrid(){
-        cells = new JButton[10][10];
-
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
                 JButton cell = new JButton();
                 cell.setFocusable(false);
                 cell.setBorderPainted(true);
@@ -46,7 +52,8 @@ public class DijkstraVisualizer {
                     cell.setEnabled(false);
                 }
                 else{
-                    cell.setBackground(new Color(245, 249, 228));
+                    cell.setBackground(cellColor);
+                    addCellListeners(cell);
                 }
 
                 cell.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
@@ -54,5 +61,61 @@ public class DijkstraVisualizer {
                 gridPanel.add(cell);
             }
         }
+    }
+
+    void addCellListeners(JButton cell) {
+
+        cell.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                isDragging = true;
+                Color currentColor = cell.getBackground();
+
+                if (currentColor.equals(cellColor)) {
+                    isAddingWalls = true;
+                    cell.setBackground(wallColor);
+                }
+                else if (currentColor.equals(wallColor)) {
+                    isAddingWalls = false;
+                    cell.setBackground(cellColor);
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                isDragging = false;
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (isDragging) {
+                    Color currentColor = cell.getBackground();
+
+                    if (currentColor.equals(Color.RED) || currentColor.equals(Color.BLUE)) {
+                        return;
+                    }
+
+                    if (isAddingWalls && currentColor.equals(cellColor)) {
+                        cell.setBackground(wallColor);
+                    }
+                    else if (!isAddingWalls && currentColor.equals(wallColor)) {
+                        cell.setBackground(cellColor);
+                    }
+                }
+            }
+        });
+
+        // Simple click
+        cell.addActionListener(e -> {
+            if (!isDragging) {
+                Color currentColor = cell.getBackground();
+                if (currentColor.equals(cellColor)) {
+                    cell.setBackground(wallColor);
+                }
+                else if (currentColor.equals(wallColor)) {
+                    cell.setBackground(cellColor);
+                }
+            }
+        });
     }
 }
